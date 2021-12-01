89839ef7ed9f4181898cea0134ba5b5c73d49b1a
Alphabug <121550810@qq.com>
2021-12-01 15:33:36 +0800

    v 1.0
    v 1.0

--- port_tunnel.go
+++ port_tunnel.go
@@ -42,7 +42,61 @@ func tcp_handle(tcpConn net.Conn){
 	go io.Copy(tcpConn,remote_tcp)
 }
 package main
+import (package main
 import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+var (
+	local = flag.String("l", "0.0.0.0:222", "0.0.0.0:222 [本地网卡监听端口]")
+	dst = flag.String("r", "8.8.8.8:1234", "8.8.8.8:1234 [NC、CobaltStrike、Metasploit等服务监听端口]")
+)
+func main() {
+	flag.Parse()
+	if len(os.Args)<=2 {
+		flag.Usage()
+	}else {
+		listen()
+	}
+
+}
+
+
+func listen()  {
+	log.Println("Start listen",*local)
+	for {
+		ln,err := net.Listen("tcp",*local)
+		if err != nil {
+			fmt.Println("tcp_listen:",err)
+			return
+		}
+		defer ln.Close()
+		for{
+			tcp_Conn,err:=ln.Accept()
+			if err!=nil{
+				fmt.Println("Accept:",err)
+				return
+			}
+			go tcp_handle(tcp_Conn)
+		}
+	}
+}
+func tcp_handle(tcpConn net.Conn){
+	remote_tcp,err:=net.Dial("tcp",*dst)
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	log.Println(dst,"=>",*local)
+	go io.Copy(remote_tcp,tcpConn)
+	log.Println(local,"=>",*dst)
+	go io.Copy(tcpConn,remote_tcp)
+
+}
 	"flag"
 	"fmt"
 	"io"
